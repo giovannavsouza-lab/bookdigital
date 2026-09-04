75854cba2d4a2d985b043867d1a6af1cabed9be6
GIOVANNA VIEIRA DE SOUZA <giovanna_v_souza@estudante.sesisenai.org.br>
2026-09-03 21:11:12 -0300

    adicionado as skins, começo das conf de skin

--- js/script.js
+++ js/script.js
@@ -6,4 +6,25 @@ function abrirLivro(){
 function voltarInicio(){
     document.getElementById("tela-principal").style.display = "none";
     document.getElementById("tela-capa").style.display = "flex";
+
+     mudarDeus(''); // Reseta os textos
+
+   /* document.getElementById("contoTitulo").innerText = "Selecione uma história";
+    document.getElementById("contoTexto").innerText = "Clique em um dos contos clássicos ao lado para ler o resumo e as observações arquivadas por Hogwarts.";
+    document.getElementById("contoNota").style.display = "none";*/
+
+    const botoes = document.querySelectorAll(".btn-conto");
+    botoes.forEach(b => b.classList.remove("active"));
+}
+/* = 2. troca das skins dos deuses = */
+
+function mudarDeus(nomeDaSkin) {
+    const container = document.getElementById("tela-principal");
+
+    // Remove qualquer classe de skin ativa anteriormente,
+    // voltando o elemento para apenas a classe base ".container"
+    container.className = "container";
+
+    // Adiciona a nova classe de skin selecionada (se houver alguma)
+    container.classList.add(nomeDaSkin);
 }
