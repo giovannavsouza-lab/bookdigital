ee6f58241d2947de4dc8ffcedf0b72a12a1f7594
GIOVANNA VIEIRA DE SOUZA <giovanna_v_souza@estudante.sesisenai.org.br>
2026-09-16 19:35:02 -0300

    falta, teoricamente o style

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
 function abrirLivro(){
     document.getElementById("tela-capa").style.display = "none";
     document.getElementById("tela-principal").style.display = "block";
-} 
+}
+
 
 function voltarInicio(){
     document.getElementById("tela-principal").style.display = "none";
     document.getElementById("tela-capa").style.display = "flex";
 
+
     const botoes = document.querySelectorAll(".btn-conto");
     botoes.forEach(b => b.classList.remove("active"));
 }
 
+
 function mudarDeus(nomeDaSkin) {
     const container = document.getElementById("tela-principal");
-  
+ 
     container.className = "container";
-    
+   
     if (nomeDaSkin) {
         container.classList.add(nomeDaSkin);
     }
 }
+
 
 const contos = [
     {
@@ -27,12 +31,13 @@ const contos = [
         titulo: "Apollo. Deus da música, medicina e do sol",
         resumo: "Segundo a mitologia grega, Apolo era filho de Zeus com Leto, uma titânide, filha de Febe e Céos. Leto engravidou de Zeus e deu à luz Apolo e Ártemis na ilha flutuante de Delos. Isso aconteceu, porque tinha ciúmes de Zeus e, enfurecida, Hera proibiu que a Mãe-Terra, Gaia, permitisse que Leto tivesse seus filhos na Terra.Para socorrer Leto, o deus Poseidon decidiu criar uma ilha flutuante chamada Delos. Como a ilha não estava presa ao solo, Gaia nada podia fazer e lá Leto deu à luz seus dois filhos. Após seu nascimento, Apolo bebeu um néctar dos deuses e comeu ambrosia e depois disso Apolo deixou de ser um bebê e tornou-se um homem adulto.Além dessa proibição, Hera também enviou a serpente Píton para que perseguisse Leto. Apolo, um exímio manejador de arco e flecha foi o responsável por matar a serpente Píton. Ele encontrou a serpente próximo ao Monte Parnaso e a matou com três flechas: uma no olho, outra no peito e outra na boca.A vitória de Apolo sobre a Píton rendeu-lhe fama e reconhecimento. A população de Delfos, local próximo ao Monte Parnaso, ficou grata por Apolo ter matado a serpente e, em homenagem ao feito, decidiu construir um templo: o Oráculo de Delfos. Lá as pessoas que veneravam Apolo iam prestar-lhe culto e obter profecias sobre o futuro"  
 
+
     },
     {
         id: 1,
         titulo: "Athena. Deusa da sabedoria",
         resumo: "Na mitologia grega, existem duas versões para narrar o nascimento de Atena. Uma das versões fala que Atena era filha de Zeus com Métis, deusa da saúde e da prudência, sendo que Métis ficou conhecida como a primeira esposa de Zeus. Depois que Métis engravidou, Gaia (personificação da Terra) teria avisado Zeus que seu filho poderia se voltar contra ele. Isso perturbou Zeus, pois ele tinha feito isso contra seu pai, Cronos. Assim, Zeus decidiu enganar Métis e a engoliu por inteiro, mas a deusa conseguiu dar à luz dentro de Zeus. Atena teria nascido ao sair da cabeça de seu pai já em sua forma adulta e munida de sua armadura e armas.Outra versão apresenta a deusa como filha exclusivamente de Zeus. Fala-se que antes do nascimento de Atena, Zeus teria sentido dores de cabeça terríveis, o que fez com que Hefesto, deus da metalurgia, abrisse a cabeça do deus supremo com um machado. Atena teria nascido depois disso. Desse modo, em ambas as versões, Atena nasceu da cabeça de Zeus."
-}, 
+},
     {
         id: 2,
         titulo: "Hermes. Mensageiro dos deuses e deus da velocidade",
@@ -45,12 +50,44 @@ const contos = [
 },
 ];
 
+
+function mudarDeus(nomeDaSkin) {
+    const container = document.getElementById("tela-principal");
+
+
+    container.className = "container";
+
+
+    container.classList.add(nomeDaSkin);
+}
+function carregarMenu() {
+    const menuContainer = document.getElementById("menuContos");
+    menuContainer.innerHTML = "";
+
+
+    contos.forEach(conto => {
+        const botao = document.createElement("button");
+        botao.classList.add("btn-conto");
+        botao.innerText = conto.titulo;
+        botao.onclick = () => exibirConto(conto.id, botao);
+        menuContainer.appendChild(botao);
+    });
+}
+
+
 function exibirConto(id, botaoSelecionado) {
     const conto = contos.find(c => c.id === id);
+
 
     if (conto) {
         document.getElementById("contoTitulo").innerText = conto.titulo;
         document.getElementById("contoTexto").innerText = conto.resumo;
+
+
+        const notaDiv = document.getElementById("contoNota");
+        notaDiv.innerText = conto.nota;
+        notaDiv.style.display = "block";
+
 
        
         const botoes = document.querySelectorAll(".btn-conto");
@@ -59,3 +96,4 @@ function exibirConto(id, botaoSelecionado) {
     }
 }
 window.onload = carregarMenu;
+
